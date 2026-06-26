95c96eaa68bb3c842e1b3ac1ae7f4d4fa9acf891
kannadati <vianvidya732@gmail.com>
2026-06-26 09:11:43 +0530

    remove duplicate provider block

--- terraform/main.tf
+++ terraform/main.tf
@@ -1,22 +1,3 @@
-terraform {
-  required_version = ">= 1.0"
-  
-  required_providers {
-    aws = {
-      source  = "hashicorp/aws"
-      version = "~> 5.0"
-    }
-  }
-}
-
-provider "aws" {
-  region = var.aws_region
-  # Credentials will be read from:
-  # 1. Environment variables (AWS_ACCESS_KEY_ID, AWS_SECRET_ACCESS_KEY)
-  # 2. AWS credentials file (~/.aws/credentials)
-  # 3. IAM role (if running on EC2/ECS)
-}
-
 # VPC for EKS
 module "vpc" {
   source = "terraform-aws-modules/vpc/aws"
